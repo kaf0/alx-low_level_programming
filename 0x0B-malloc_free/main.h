580ab2769cc2971df2a89318489e52f11a4894f7
“kaf0” <gertrudeseworh@gmail.com>
2022-10-03 10:47:21 -0700

    Task 0 to Task 6

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+int _strlen(char *s);
+
+char *str_concat(char *s1, char *s2);
+
+char *_strncat(char *dest, char *src, int n);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+#endif
