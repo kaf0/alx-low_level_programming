36581549e2e2ae96f8c9b83fc59ed3edd019b945
“kaf0” <gertrudeseworh@gmail.com>
2022-10-06 08:30:10 -0700

    Task 0 fix

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,22 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _puts(char *str);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-
-char *create_array(unsigned int size, char c);
-
-char *_strdup(char *str);
-
-int _strlen(char *s);
-
-char *str_concat(char *s1, char *s2);
-
-char *_strncat(char *dest, char *src, int n);
-
-int **alloc_grid(int width, int height);
-
-void free_grid(int **grid, int height);
-
-char *argstostr(int ac, char **av);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
